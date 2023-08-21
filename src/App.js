5c52831315dd53ca42b93ab2a6e54075e4591ce5
joderberg <josefanderberg@MacBook-Pro-som-tillhor-Josef.local>
2023-08-21 14:09:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="App" onMouseDown={startDrag} onMouseUp={endDrag} onMouseMove={move}>
+              <button className="CreateButton" onClick={addCircle}>SKAPA</button>
+              <button className="CreateButton" style={{left: 'calc(20% + 20px)'}} onClick={toggleModal}>SYMBOL</button>
+
+              {showModal && (
+                  <div className="modal">
+                      <label>Color: <input type="color" value={starColor} onChange={(e) => setStarColor(e.target.value)} /></label>
+                      <label>Size: <input type="number" value={starSize} onChange={(e) => setStarSize(e.target.value)} /></label>
+                      <label>Price: <input type="number" value={starPrice} onChange={(e) => setStarPrice(e.target.value)} /></label>
+                      <button onClick={toggleModal}>Close</button>
+                  </div>
+              )}
+            {circles.map((circle, index) => (
+                <div
+                    key={index}
+                    className="Circle"
+                    style={{
+                        left: circle.x - circle.radius,
+                        top: circle.y - circle.radius,
+                        width: circle.radius * 5,
+                        height: circle.radius * 5
+                    }}
+                    onMouseDown={(e) => startDragCircle(e, index)}
+                    onClick={() => toggleGrid(index)}
+                >
           Learn React
         </a>
       </header>
